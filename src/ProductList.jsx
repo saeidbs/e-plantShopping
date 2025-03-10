879cef63406be498cbdbc55fc7a6ab3ee7233557
Skewfield <saeidbahmani1377@gmail.com>
2025-03-10 01:09:09 -0300

    done

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -292,8 +292,9 @@ function ProductList({ onHomeClick }) {
 
                 </div>
                 <div style={styleObjUl}>
-                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1>{totalItems > 0 && ( <span style={{ position: 'absolute', top: '10px', right: '10px', backgroundColor: 'red', color: 'white', borderRadius: '50%', padding: '4px 4px', fontSize: '12px' }}> {totalItems} </span> )}</a></div>
+                    <div> <a href="#" style={styleA}>Plants</a></div>
+                    <div> <a href="#" style={styleA} onClick={handleCartClick}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="16"></circle><circle cx="184" cy="216" r="16"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg>{totalItems > 0 && <span className="cart_quantity_count">{totalItems}</span>}
+                    </h1></a></div>
                 </div>
             </div>
             {!showCart ? (
